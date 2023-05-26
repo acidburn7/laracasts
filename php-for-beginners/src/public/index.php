54ed91a7713852e36663845ab0d5160e575f3413
acidburn7 <varaksinaalla@mail.ru>
2023-05-26 22:09:38 +0300

    separate logic from the template

--- php-for-beginners/src/public/index.php
+++ php-for-beginners/src/public/index.php
@@ -1,48 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Recommended books</h1>
-    <?php
-        $books = [
-            [
-                'name' => 'Do Androids of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'http://example.com'
-            ]
-        ];
+<?php
+$books = [
+    [
+        'name' => 'Do Androids of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ]
+];
 
-        $filteredBooks = array_filter($books, function($book) {
-            return $book['releaseYear'] == 1968;
-        });
-    ?>
-    <ul>
-        <?php foreach($filteredBooks as $book): ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
-                    by <?= $book['author'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+$filteredBooks = array_filter($books, function($book) {
+    return $book['releaseYear'] == 1968;
+});
+
+require "index.view.php";
