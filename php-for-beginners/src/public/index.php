7f08749bb6a830836ec351f34d4b8cf5611f093a
acidburn7 <varaksinaalla@mail.ru>
2023-05-27 12:41:08 +0300

    current page styling

--- php-for-beginners/src/public/index.php
+++ php-for-beginners/src/public/index.php
@@ -1,29 +1,14 @@
 <?php
 
 $heading = "Home";
-$books = [
-    [
-        'name' => 'Do Androids of Electric Sheep',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        'name' => 'Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'http://example.com'
-    ]
-];
 
-$filteredBooks = array_filter($books, function($book) {
-    return $book['releaseYear'] == 1968;
-});
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
 
+    die();
+}
+
+require "functions.php";
 require "views/index.view.php";
