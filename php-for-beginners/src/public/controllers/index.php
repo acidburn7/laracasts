324e020c507caf906c7c3fec42003e6a4a998601
acidburn7 <varaksinaalla@mail.ru>
2023-06-17 22:32:38 +0300

    Make a PHP Router

--- php-for-beginners/src/public/controllers/index.php
+++ php-for-beginners/src/public/controllers/index.php
@@ -2,13 +2,4 @@
 
 $heading = "Home";
 
-function dd($value) {
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
-
-require "functions.php";
 require "views/index.view.php";
